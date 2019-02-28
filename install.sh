8046bf47803e6be576552906d7ff1eea15d9465a
Embedded@Safepro <45988949+surendrasafepro@users.noreply.github.com>
2019-02-28 22:54:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ echo 'System had been updated'
 
 # Mjpg-Streamer
 # Installing the Mjpg-Streamer for camera streaming
+cd ~/
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer/mjpg-streamer-experimental
 make
@@ -21,22 +22,29 @@ sudo cp -r output_http.so input_uvc.so /usr/local/lib/
 echo 'Mjpg-streamer had been installed successfully'
 
 # coding for Camera streaming
+cd ~/
 mkdir sniffer
+cd sniffer/
 mkdir shell
-cd /sniffer/shell/
+cd shell/
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/stream.sh
 
 # Coding for Sniffer motion
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/int.sh
-wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/int.sh
+wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/fwd.sh
+wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/bwd.sh
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/rgt.sh
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/lft.sh
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/stop.sh
 
-# INstalling Apache2 and PHP7
+echo 'Coding for motion has been successfully done.'
+
+# Installing Apache2 and PHP7
+cd ~/
 sudo apt-get install php libapache2-mod-php
 sudo a2enmod mpm_prefork && sudo a2enmod php7.0
 sudo service apache2 restart
+echo 'Apache and PHP had been installed successfully'
 
 # Proving permissions for PHP to access the files
 sudo sh -c 'echo "www-data ALL=NOPASSWD: ALL" >> sudo visudo'
@@ -46,6 +54,8 @@ cd /var/www/html/
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/sniffer.php
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/index.html
 wget https://raw.githubusercontent.com/surendrasafepro/Sniffer-TX2/master/jquery.min.js
+
+echo 'Hoory..! Successfully installation done.'
 
 
 
