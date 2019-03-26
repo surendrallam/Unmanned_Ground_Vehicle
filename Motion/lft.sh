a66f622594eaafd33fdc5d5e6bc4ec0e1b6afee3
Embedded@Safepro <45988949+surendrasafepro@users.noreply.github.com>
2019-03-26 15:26:00 +0530

    Update lft.sh

--- Motion/lft.sh
+++ Motion/lft.sh
@@ -1,11 +1,11 @@
 #!/bin/sh 
 
-La=389
-Lb=395
-Ra=392
-Rb=255
+Lp=389 # PWM pin of Left
+Ld=395 # Direction pin of Left
+Rp=392
+Rd=255
 
-echo 1 > /sys/class/gpio/gpio$La/value 
-echo 0 > /sys/class/gpio/gpio$Lb/value 
-echo 1 > /sys/class/gpio/gpio$Ra/value 
-echo 0 > /sys/class/gpio/gpio$Rb/value
+echo 1 > /sys/class/gpio/gpio$Lp/value 
+echo 0 > /sys/class/gpio/gpio$Ld/value 
+echo 1 > /sys/class/gpio/gpio$Rp/value 
+echo 0 > /sys/class/gpio/gpio$Rd/value
